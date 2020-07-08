8a16aeb22c96a65395653cfbe78f39f679fb8b1a
hunter foulk <huntertehjakey@hotmail.com>
2020-07-08 14:21:04 -0700

    dev ops

--- src/routes/portfolio.js
+++ src/routes/portfolio.js
@@ -34,12 +34,11 @@ router.post("/sendemail", async (req, res) => {
 
   console.log("this is the body", req.body);
 
-  exports.handler = function (event, context, callback) {
-    let mailOptions = {
-      to: "hunterfoulkdev@gmail.com",
-      from: "huntertehjakey@hotmail.com",
-      subject: `New Inquiry (${name})`,
-      html: `
+  let mailOptions = {
+    to: "hunterfoulkdev@gmail.com",
+    from: "huntertehjakey@hotmail.com",
+    subject: `New Inquiry (${name})`,
+    html: `
       <table style="max-width: 700px; width: 100%;">
       <tr>
         <td>
@@ -72,42 +71,34 @@ router.post("/sendemail", async (req, res) => {
       </tr>
     </table>
     `,
-    };
-
-    let transporter = nodemailer.createTransport(
-      smtpTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        auth: {
-          user: "hunterfoulkdev@gmail.com",
-          pass: "Hunterfoulk01",
-        },
-        tls: {
-          ciphers: "SSLv3",
-        },
-      })
-    );
-
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        const response = {
-          statusCode: 500,
-          body: JSON.stringify({
-            error: error.message,
-          }),
-        };
-        callback(null, response);
-      }
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-          message: `Email processed succesfully!`,
-        }),
-      };
-      callback(null, response);
-    });
   };
+
+  let transporter = nodemailer.createTransport(
+    smtpTransport({
+      host: "smtp.gmail.com",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "hunterfoulkdev@gmail.com",
+        pass: "Hunterfoulk01",
+      },
+      tls: {
+        ciphers: "SSLv3",
+      },
+    })
+  );
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (!error) {
+      console.log("ID -> ", info.messageId);
+      console.log("Sender -> ", email);
+      console.log("Receiver -> ", info.envelope.to);
+      res.status(200).send("Email Sent");
+    } else {
+      console.log(error);
+      res.status(400).send();
+    }
+  });
 });
 
 module.exports = router;
